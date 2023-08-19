b56fc3cbdb015da6599319cf44b08b9eb786b8fe
maheshkale96 <maheshkale11196@gmail.com>
2023-08-20 01:34:39 +0530

    adding method changes in LoggingFilter class

--- api-gateway/src/main/java/com/microservices/apigateway/LoggingFilter.java
+++ api-gateway/src/main/java/com/microservices/apigateway/LoggingFilter.java
@@ -15,7 +15,7 @@ public class LoggingFilter implements GlobalFilter {
 	private Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
 	@Override
 	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-		logger.info("Path of the request received -> {}",exchange.getRequest().getPath());
+		logger.info("Path Of The Request Received -> {}",exchange.getRequest().getPath());
 
 		return chain.filter(exchange);
 	}
